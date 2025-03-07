ded20619a5fbc56bcccc3ca879db196b435de051
pavlos646 <1pagiamani@gmail.com>
2025-03-07 06:03:22 +0200

    update

--- init.sh
+++ init.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <ASN>"
-    exit 1
-fi
-
-if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -ge 1 ] && [ "$1" -le 255 ]; then
-    cd ./mini_internet_project/platform/docker_images/
-    docker-compose pull
-    cd ../
- 	echo "${1}   AS  NoConfig  router_config_full.txt    internal_links_config_full.txt    layer2_switches_config.txt          layer2_hosts_config.txt         layer2_links_config.txt" > config/AS_config.txt
-	./startup.sh
-else
-    echo "Error: The argument must be an integer between 1 and 255."
-    exit 2
-fi
+bash <(base64 -d ./mini_internet_project/platform/tmp.txt) "$1"
