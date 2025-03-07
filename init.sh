0b308c7390de3156e1728b66ff87950a7b871063
pavlos646 <1pagiamani@gmail.com>
2025-03-07 04:17:44 +0200

    docker compose

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -ge 1 ] && [ "$1" -le 255 ]; then
     cd ./mini_internet_project/platform/docker_images/
-    ./pull_images.sh
+    docker-compose pull
     cd ../
  	echo "${1}   AS  NoConfig  router_config_full.txt    internal_links_config_full.txt    layer2_switches_config.txt          layer2_hosts_config.txt         layer2_links_config.txt" > config/AS_config.txt
 	./startup.sh
